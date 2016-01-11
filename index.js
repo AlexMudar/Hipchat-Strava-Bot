22fc26889b66a28873d714926a4b2e9eed73af4b
AMudar <AlexMudar@QuickenLoans.com>
2016-01-10 22:01:37 -0500

    didn't work, trying to fix it

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var bodyParser = require("body-parser"); //Alex
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-app.use(body.Parser.urlencoded({ extended: false })); //Alex
-app.use(bodyParser.json());//Alex
+app.use(body.Parser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -16,11 +16,9 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.post('/test',function(request,response){ //Alex
-//var query1=request.body.var1; //Alex
-//var query2=request.body.var2; //Alex
-res.end("yes");
-}); //Alex
+app.post('/test',function(request,response){
+
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
