f484b4de6551c82e3eab1657b5a8982664a17df8
AlexMudar <AlexMudar@gmail.com>
2016-02-05 22:23:51 -0500

    clean up

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
 	response.render('pages/index');
